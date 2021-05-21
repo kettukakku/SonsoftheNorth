fdfdde3dcbec5f7b11f9a544b63d7399191a5cb1
Rien <darienkirst@gmail.com>
2021-05-21 00:02:10 -0400

    Adding unity project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveMethod = x + speed;
+        rb.velocity = new Vector2(moveMethod, rb.velocity.y);
+    }
+}
